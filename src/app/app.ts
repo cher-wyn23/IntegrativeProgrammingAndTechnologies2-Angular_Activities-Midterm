74af39bf84bd6cdd1205aaf465d3ff28b43e57d1
cher-wyn23 <cherwyn.malquisto@evsu.edu.ph>
2026-02-20 14:13:53 +0800

    Updated login form and CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,45 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('My-Login');
+
+  username = '';
+  password = '';
+
+  passwordVisible = false; 
+
+  message = '';
+  error = '';
+
+  correctUsername = 'JOHN ROQUE B. ABINA';
+  correctPassword = '2023-00145'; 
+
+  togglePassword() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  onLogin() {
+    const enteredUser = this.username.trim().toUpperCase();
+    const correctUser = this.correctUsername.trim().toUpperCase();
+
+    const enteredPass = this.password.trim();
+
+
+    if (enteredUser === correctUser && enteredPass === this.correctPassword) {
+      this.error = '';
+      this.message =
+        `Welcome, ${this.correctUsername}! You have successfully logged in. Have a productive learning experience in Integrative Programming and Technologies 2.`;
+    } else {
+      this.message = '';
+      this.error = 'Invalid username or password. Please try again.';
+    }
+  }
 }
