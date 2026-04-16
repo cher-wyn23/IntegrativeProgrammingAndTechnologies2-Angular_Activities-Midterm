11ac5062c1aba926af7eae3d5190d6ae9f27d3d3
cher-wyn23 <cherwyn.malquisto@evsu.edu.ph>
2026-04-16 23:10:35 +0800

    Activity5

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,6 +10,8 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.css']
 })
 export class App {
+  isLoggedIn = false;
+  currentUserName = '';
   email = '';
   password = '';
   rememberMe = false;
@@ -21,6 +23,15 @@ export class App {
 
   correctEmail = 'cherwyn@gmail.com';
   correctPassword = '12345678'; 
+
+  private formatDisplayName(email: string): string {
+    const localPart = email.split('@')[0] ?? 'Guest';
+    return localPart
+      .split(/[._-]/)
+      .filter(Boolean)
+      .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+      .join(' ');
+  }
 
   togglePassword() {
     this.passwordVisible = !this.passwordVisible;
@@ -52,11 +63,25 @@ export class App {
     if (enteredEmail === correctEmail && enteredPass === this.correctPassword) {
       this.error = '';
       this.message = 'Welcome back to Oceanic Retreats. Your coastal getaway dashboard is ready.';
+      this.isLoggedIn = true;
+      this.currentUserName = this.formatDisplayName(enteredEmail);
     } else {
       this.message = '';
       this.error = 'Invalid email or password. Please try again.';
+      this.isLoggedIn = false;
+      this.currentUserName = '';
     }
 
     this.isSubmitting = false;
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.currentUserName = '';
+    this.email = '';
+    this.password = '';
+    this.rememberMe = false;
+    this.passwordVisible = false;
+    this.clearFeedback();
   }
 }
