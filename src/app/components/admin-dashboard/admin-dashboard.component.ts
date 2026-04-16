8248f777ad2f4509897991880575b9b9ca4e16fa
cher-wyn23 <cherwyn.malquisto@evsu.edu.ph>
2026-04-16 22:34:44 +0800

    Activity8

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
   templateUrl: './admin-dashboard.component.html',
 })
 export class AdminDashboardComponent {
